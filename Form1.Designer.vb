f19f25c752df0c78e99d2b78e6b5a136b0507c47
dileepPonnam <ponnamdileep456@gmail.com>
2017-03-10 21:31:58 -0500

    Car Payment
    Create an application with 1 form.  On this form I want you to calculate
    a monthly car payment for buying a new car.  You need to take in the
    following as user input:  Name of the vehicle (Ford Mustang for
    example), Purchase Price, Sales Tax Amount, and Number of Years for the
    loan.
    
    You will need to have a button to calculate the result, clear the user
    input, and exit the application.  When the user hits the calculate
    button you need to display the car name and the result of the car
    payment in read-only text boxes or a labels (your choice).
    
    When the user hits the Clear button, you need to clear the user’s entry
    out of the text boxes.  When the user hits the exit button, the
    application needs to close.
    
    Please use variables with the correct data type to capture the user’s
    input.  To simplify the calculation, assume no annual interest rate and
    use this formula: Car Payment = (Purchase Price * Sales Tax) / (Years *
    12).
    
    Please also include a Status Strip control at the bottom of the form
    with a Status Label that displays the current date and time when the
    form loads (Use the Forms On-Load event).
    
    How you design the form and name the controls and variables is up to,
    but make the layout make sense.

---
+++ Form1.Designer.vb
@@ -0,0 +1,260 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.lblDateToday = New System.Windows.Forms.TextBox()
+        Me.lblTimeToday = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.txtName = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.txtprice = New System.Windows.Forms.TextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.txtloan = New System.Windows.Forms.TextBox()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.lblTax = New System.Windows.Forms.TextBox()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.lblPayment = New System.Windows.Forms.TextBox()
+        Me.Calculate = New System.Windows.Forms.Button()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.StatusStrip1 = New System.Windows.Forms.StatusStrip()
+        Me.ToolStripStatusLabel1 = New System.Windows.Forms.ToolStripStatusLabel()
+        Me.ToolStripStatusLabel2 = New System.Windows.Forms.ToolStripStatusLabel()
+        Me.StatusStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(18, 19)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(70, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Today's Date"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(18, 58)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(70, 13)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Today's Time"
+        '
+        'lblDateToday
+        '
+        Me.lblDateToday.Location = New System.Drawing.Point(181, 16)
+        Me.lblDateToday.Name = "lblDateToday"
+        Me.lblDateToday.Size = New System.Drawing.Size(195, 20)
+        Me.lblDateToday.TabIndex = 2
+        '
+        'lblTimeToday
+        '
+        Me.lblTimeToday.Location = New System.Drawing.Point(182, 56)
+        Me.lblTimeToday.Name = "lblTimeToday"
+        Me.lblTimeToday.Size = New System.Drawing.Size(193, 20)
+        Me.lblTimeToday.TabIndex = 3
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(23, 120)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(84, 13)
+        Me.Label3.TabIndex = 4
+        Me.Label3.Text = "Name of the Car"
+        '
+        'txtName
+        '
+        Me.txtName.Location = New System.Drawing.Point(184, 111)
+        Me.txtName.Name = "txtName"
+        Me.txtName.Size = New System.Drawing.Size(190, 20)
+        Me.txtName.TabIndex = 5
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(30, 185)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(78, 13)
+        Me.Label4.TabIndex = 6
+        Me.Label4.Text = "Purchase price"
+        '
+        'txtprice
+        '
+        Me.txtprice.Location = New System.Drawing.Point(185, 180)
+        Me.txtprice.Name = "txtprice"
+        Me.txtprice.Size = New System.Drawing.Size(190, 20)
+        Me.txtprice.TabIndex = 7
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(30, 251)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(140, 13)
+        Me.Label5.TabIndex = 8
+        Me.Label5.Text = "Number of years for the loan"
+        '
+        'txtloan
+        '
+        Me.txtloan.Location = New System.Drawing.Point(211, 249)
+        Me.txtloan.Name = "txtloan"
+        Me.txtloan.Size = New System.Drawing.Size(204, 20)
+        Me.txtloan.TabIndex = 9
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(38, 310)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(54, 13)
+        Me.Label6.TabIndex = 10
+        Me.Label6.Text = "Sales Tax"
+        '
+        'lblTax
+        '
+        Me.lblTax.Location = New System.Drawing.Point(208, 310)
+        Me.lblTax.Name = "lblTax"
+        Me.lblTax.Size = New System.Drawing.Size(206, 20)
+        Me.lblTax.TabIndex = 11
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(25, 372)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(82, 13)
+        Me.Label7.TabIndex = 12
+        Me.Label7.Text = "Montly Payment"
+        '
+        'lblPayment
+        '
+        Me.lblPayment.Location = New System.Drawing.Point(201, 370)
+        Me.lblPayment.Name = "lblPayment"
+        Me.lblPayment.Size = New System.Drawing.Size(213, 20)
+        Me.lblPayment.TabIndex = 13
+        '
+        'Calculate
+        '
+        Me.Calculate.Location = New System.Drawing.Point(59, 446)
+        Me.Calculate.Name = "Calculate"
+        Me.Calculate.Size = New System.Drawing.Size(110, 30)
+        Me.Calculate.TabIndex = 14
+        Me.Calculate.Text = "Calculate"
+        Me.Calculate.UseVisualStyleBackColor = True
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(211, 447)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(115, 28)
+        Me.Button1.TabIndex = 15
+        Me.Button1.Text = "Clear"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(375, 446)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(126, 28)
+        Me.Button2.TabIndex = 16
+        Me.Button2.Text = "Exit"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'StatusStrip1
+        '
+        Me.StatusStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ToolStripStatusLabel1, Me.ToolStripStatusLabel2})
+        Me.StatusStrip1.Location = New System.Drawing.Point(0, 471)
+        Me.StatusStrip1.Name = "StatusStrip1"
+        Me.StatusStrip1.Size = New System.Drawing.Size(539, 22)
+        Me.StatusStrip1.TabIndex = 17
+        Me.StatusStrip1.Text = "StatusStrip1"
+        '
+        'ToolStripStatusLabel1
+        '
+        Me.ToolStripStatusLabel1.Name = "ToolStripStatusLabel1"
+        Me.ToolStripStatusLabel1.Size = New System.Drawing.Size(31, 17)
+        Me.ToolStripStatusLabel1.Text = "Date"
+        '
+        'ToolStripStatusLabel2
+        '
+        Me.ToolStripStatusLabel2.Name = "ToolStripStatusLabel2"
+        Me.ToolStripStatusLabel2.Size = New System.Drawing.Size(34, 17)
+        Me.ToolStripStatusLabel2.Text = "Time"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(539, 493)
+        Me.Controls.Add(Me.StatusStrip1)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Calculate)
+        Me.Controls.Add(Me.lblPayment)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.lblTax)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.txtloan)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.txtprice)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.txtName)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.lblTimeToday)
+        Me.Controls.Add(Me.lblDateToday)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Car monthly payment"
+        Me.StatusStrip1.ResumeLayout(False)
+        Me.StatusStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents lblDateToday As System.Windows.Forms.TextBox
+    Friend WithEvents lblTimeToday As System.Windows.Forms.TextBox
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents txtName As System.Windows.Forms.TextBox
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents txtprice As System.Windows.Forms.TextBox
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents txtloan As System.Windows.Forms.TextBox
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+    Friend WithEvents lblTax As System.Windows.Forms.TextBox
+    Friend WithEvents Label7 As System.Windows.Forms.Label
+    Friend WithEvents lblPayment As System.Windows.Forms.TextBox
+    Friend WithEvents Calculate As System.Windows.Forms.Button
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents StatusStrip1 As System.Windows.Forms.StatusStrip
+    Friend WithEvents ToolStripStatusLabel1 As System.Windows.Forms.ToolStripStatusLabel
+    Friend WithEvents ToolStripStatusLabel2 As System.Windows.Forms.ToolStripStatusLabel
+
+End Class
---
+++ Form1.vb
@@ -0,0 +1,58 @@
+﻿Public Class Form1
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        ' Get today’s date from the system and display it.
+        lblDateToday.Text = Now.ToString("D")
+
+        ' Get the current time from the system and display it.
+        lblTimeToday.Text = Now.ToString("T")
+    End Sub
+
+    Private Sub Calculate_Click(sender As Object, e As EventArgs) Handles Calculate.Click
+        Dim decTax As Decimal
+        Dim decPayment As Decimal
+        Const decTAX_RATE As Decimal = 0.08  ' Tax rate
+        Try
+            'Calculate the Sales Tax
+            decTax = CDec(txtprice.Text) * decTAX_RATE
+            lblTax.Text = decTax.ToString("c")
+
+            'calculate the monthly payment
+            decPayment = (CDec(txtprice.Text) + decTax) / ((txtloan.Text) * 12)
+            lblPayment.Text = txtName.Text & decPayment.ToString("c")
+        Catch ex As Exception
+            ' Error message
+            MessageBox.Show("All input must be valid numeric values.")
+        End Try
+
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        'clear the fields
+        txtName.Clear()
+        txtprice.Clear()
+        txtloan.Clear()
+        'clearing the monthly and tax fields
+        lblTax.Text = String.Empty
+        lblPayment.Text = String.Empty
+
+        txtName.Focus()
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Me.Close()
+
+    End Sub
+
+    Private Sub ToolStripStatusLabel1_Click(sender As Object, e As EventArgs) Handles ToolStripStatusLabel1.Click
+        lblDateToday.Text = Now.ToString("D")
+
+    End Sub
+
+    Private Sub ToolStripStatusLabel2_Click(sender As Object, e As EventArgs) Handles ToolStripStatusLabel2.Click
+        lblTimeToday.Text = Now.ToString("T")
+    End Sub
+End Class
+
